a5147338a5a6a5f6ff6522226ae3ae0b7d84014d
Chanyeol Park <1qkrcksduf1@naver.com>
2024-03-17 20:14:45 +0900

    feat: getQuotation

--- src/main/kotlin/wisoft/io/quotation/adaptor/in/http/QuotationController.kt
+++ src/main/kotlin/wisoft/io/quotation/adaptor/in/http/QuotationController.kt
@@ -1,16 +1,23 @@
 package wisoft.io.quotation.adaptor.`in`.http
 
+import jakarta.websocket.server.PathParam
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import wisoft.io.quotation.application.port.`in`.GetQuotationUseCase
 import wisoft.io.quotation.application.port.`in`.GetQuotationsUseCase
+import java.util.UUID
 
 @RestController
 @RequestMapping("/quotations")
-class QuotationController(val getQuotationsUseCase: GetQuotationsUseCase) {
+class QuotationController(
+    val getQuotationsUseCase: GetQuotationsUseCase,
+    val getQuotationUseCase: GetQuotationUseCase
+) {
 
     @GetMapping
     fun getQuotations(@ModelAttribute request: GetQuotationsUseCase.GetQuotationRequest):
@@ -18,5 +25,13 @@ class QuotationController(val getQuotationsUseCase: GetQuotationsUseCase) {
         val response = getQuotationsUseCase.getQuotations(request)
         return ResponseEntity.status(HttpStatus.OK)
             .body(GetQuotationsUseCase.GetQuotationResponse(data = GetQuotationsUseCase.Data(quotations = response)))
+    }
+
+    @GetMapping("/{id}")
+    fun getQuotation(@PathVariable("id") id: UUID): ResponseEntity<GetQuotationUseCase.GetQuotationResponse> {
+        println("들어옴")
+        val response = getQuotationUseCase.getQuotation(id)
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(GetQuotationUseCase.GetQuotationResponse(data = GetQuotationUseCase.Data(response)))
     }
 }
