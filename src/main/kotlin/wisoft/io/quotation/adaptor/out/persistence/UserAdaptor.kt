b0c963ce4155ebd22c1e828f855f9837c045f979
Kukjun Lee <lxx3380@gmail.com>
2024-03-19 16:36:25 +0900

    fix : seperate port responsibility
    port 책임을 단일 메서드로 분리함

--- src/main/kotlin/wisoft/io/quotation/adaptor/out/persistence/UserAdaptor.kt
+++ src/main/kotlin/wisoft/io/quotation/adaptor/out/persistence/UserAdaptor.kt
@@ -1,16 +1,15 @@
 package wisoft.io.quotation.adaptor.out.persistence
 
-import org.springframework.data.domain.Example
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import wisoft.io.quotation.adaptor.out.persistence.entity.UserEntity
 import wisoft.io.quotation.adaptor.out.persistence.repository.UserRepository
-import wisoft.io.quotation.application.port.out.FindUserPort
-import wisoft.io.quotation.application.port.out.SaveUserPort
+import wisoft.io.quotation.application.port.out.*
 import wisoft.io.quotation.domain.User
 
 @Component
-class UserAdaptor(val userRepository: UserRepository) : SaveUserPort, FindUserPort {
+class UserAdaptor(val userRepository: UserRepository) : SaveUserPort, FindUserByIdPort, FindLeaveUserListCountPort,
+    ExistUserPort, ExistUserByNicknamePort {
 
     override fun save(user: User): String {
         return userRepository.save(UserEntity.from(user)).id
