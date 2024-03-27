27406397b8105dc46053c712a34ec46ae0fec01d
Chanyeol Park <1qkrcksduf1@naver.com>
2024-03-27 16:39:24 +0900

    fix: test code import

--- src/test/kotlin/wisoft/io/quotation/integration/http/adaptor/in/QuotationControllerTest.kt
+++ src/test/kotlin/wisoft/io/quotation/integration/http/adaptor/in/QuotationControllerTest.kt
@@ -1,4 +1,4 @@
-package wisoft.io.quotation.adaptor.`in`.http
+package wisoft.io.quotation.integration.http.adaptor.`in`
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
