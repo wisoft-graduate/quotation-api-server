345e184fb7e5a9fdb43c68659acf2880c32df72e
Kukjun Lee <lxx3380@gmail.com>
2024-04-25 13:37:30 +0900

    fix : getUserList
    jdsl을 이용해서 getUserList의 요청 파람의 정보들로 Repository 영역에서 필터링하도록 구현함

--- src/main/kotlin/wisoft/io/quotation/adaptor/out/persistence/UserAdaptor.kt
+++ src/main/kotlin/wisoft/io/quotation/adaptor/out/persistence/UserAdaptor.kt
@@ -2,7 +2,7 @@ package wisoft.io.quotation.adaptor.out.persistence
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
-import wisoft.io.quotation.adaptor.out.persistence.entity.UserEntity
+import wisoft.io.quotation.adaptor.out.persistence.repository.UserCustomRepository
 import wisoft.io.quotation.adaptor.out.persistence.repository.UserRepository
 import wisoft.io.quotation.application.port.`in`.GetUserListUseCase
 import wisoft.io.quotation.application.port.out.*
@@ -11,6 +11,7 @@ import wisoft.io.quotation.domain.User
 @Component
 class UserAdaptor(
     val userRepository: UserRepository,
+    val userCustomRepository: UserCustomRepository,
 ) : GetUserPort, CreateUserPort, UpdateUserPort, DeleteUserPort, GetUserListPort {
     override fun create(user: User): String {
         return userRepository.save(user.toEntity()).id
@@ -35,13 +36,7 @@ class UserAdaptor(
     }
 
     override fun getUserList(request: GetUserListUseCase.GetUserListRequest): List<User> {
-        val userList: List<UserEntity> =
-            when {
-                request.searchNickname != null -> userRepository.findAllByNicknameContains(request.searchNickname)
-                request.nickname != null -> userRepository.findAllByNickname(request.nickname)
-                request.id != null -> userRepository.findAllById(request.id)
-                else -> emptyList() // 모든 옵션이 없는 경우 빈 리스트 반환
-            }
+        val userList = userCustomRepository.getUserList(request)
         return userList.map { it.toDomain() }
     }
 
