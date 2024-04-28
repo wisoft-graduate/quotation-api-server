965676cdec83498768df7fb658009c9a7332c050
Kukjun Lee <lxx3380@gmail.com>
2024-04-28 23:24:32 +0900

    fix : duplicate exception
    중복에 대한 명확한 메시지를 작성하고 현재시간 표시에 대한 오류를 수정함

--- src/main/kotlin/wisoft/io/quotation/exception/error/UserDuplicateException.kt
+++ src/main/kotlin/wisoft/io/quotation/exception/error/UserDuplicateException.kt
@@ -1,5 +1,5 @@
 package wisoft.io.quotation.exception.error
 
-import wisoft.io.quotation.exception.error.http.BadRequestException
+import wisoft.io.quotation.exception.error.http.DuplicateException
 
-class UserDuplicateException(override val value: String) : BadRequestException(value = value)
+class UserDuplicateException(override val value: String) : DuplicateException(value = value)
