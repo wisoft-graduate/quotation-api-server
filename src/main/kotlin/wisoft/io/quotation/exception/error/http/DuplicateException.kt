189d759a73e54476f3287f5ad511892e349e81c7
Kukjun Lee <lxx3380@gmail.com>
2024-05-08 11:30:03 +0900

    fix: duplicateException
    예외 메시지 변경

--- src/main/kotlin/wisoft/io/quotation/exception/error/http/DuplicateException.kt
+++ src/main/kotlin/wisoft/io/quotation/exception/error/http/DuplicateException.kt
@@ -3,5 +3,5 @@ package wisoft.io.quotation.exception.error.http
 open class DuplicateException(
     open val value: String,
 ) : Throwable(
-        message = value + HttpMessage.HTTP_400_DUPLICATE.message,
+        message = HttpMessage.HTTP_400_DUPLICATE.message.replace("자원", value),
     )
--- src/main/kotlin/wisoft/io/quotation/exception/error/http/HttpMessage.kt
+++ src/main/kotlin/wisoft/io/quotation/exception/error/http/HttpMessage.kt
@@ -7,7 +7,7 @@ enum class HttpMessage(
     val message: String,
 ) {
     HTTP_400(HttpStatus.BAD_REQUEST, " 잘못된 요청입니다."),
-    HTTP_400_DUPLICATE(HttpStatus.BAD_REQUEST, " 중복 자원입니다."),
+    HTTP_400_DUPLICATE(HttpStatus.BAD_REQUEST, "중복된 자원 입니다."),
     HTTP_401(HttpStatus.UNAUTHORIZED, " 인증되지 않은 사용자입니다."),
     HTTP_403(HttpStatus.FORBIDDEN, " 사용자는 콘텐츠에 접근할 권리를 가지고 있지 않습니다."),
     HTTP_404(HttpStatus.NOT_FOUND, " 리소스가 존재하지 않습니다."),
