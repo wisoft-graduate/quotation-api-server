6e547daf9f467c23fb4a3fa96fe4aa32bcb2dec3
Chanyeol Park <1qkrcksduf1@naver.com>
2024-05-19 16:09:09 +0900

    feat: CORS 적용

---
+++ src/main/kotlin/wisoft/io/quotation/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package wisoft.io.quotation.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
