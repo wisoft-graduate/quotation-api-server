d0ae9319ba2260f78b85967b91c35348c8b376aa
Chanyeol Park <42057170+qkrcksduf@users.noreply.github.com>
2024-06-09 19:01:13 +0900

    fix: 스웨거 통해서 인증 가능하도록 수정 (#84)

---
+++ src/main/kotlin/wisoft/io/quotation/config/SwaggerConfig.kt
@@ -0,0 +1,42 @@
+package wisoft.io.quotation.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI()
+            .components(
+                Components().addSecuritySchemes(
+                    "BearerToken",
+                    SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"),
+                ),
+            )
+            .addSecurityItem(SecurityRequirement().addList("BearerToken"))
+            .info(
+                Info()
+                    .title("Things API")
+                    .version("1.0.0")
+                    .description("API documentation with Springdoc OpenAPI"),
+            )
+    }
+
+    @Bean
+    fun publicApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("public")
+            .pathsToMatch("/**")
+            .build()
+    }
+}
