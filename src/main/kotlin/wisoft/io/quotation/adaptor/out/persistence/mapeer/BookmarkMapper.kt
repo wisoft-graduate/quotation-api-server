11b6b742b0456efccb46e3673a74e43b77fe9d76
Chanyeol Park <1qkrcksduf1@naver.com>
2024-07-22 13:35:55 +0900

    fix: getBookmark

--- src/main/kotlin/wisoft/io/quotation/adaptor/out/persistence/mapeer/BookmarkMapper.kt
+++ src/main/kotlin/wisoft/io/quotation/adaptor/out/persistence/mapeer/BookmarkMapper.kt
@@ -2,16 +2,20 @@ package wisoft.io.quotation.adaptor.out.persistence.mapeer
 
 import org.springframework.stereotype.Component
 import wisoft.io.quotation.adaptor.out.persistence.entity.BookmarkEntity
+import wisoft.io.quotation.adaptor.out.persistence.repository.QuotationRepository
 import wisoft.io.quotation.domain.Bookmark
 
 @Component
-class BookmarkMapper : Mapper<BookmarkEntity, Bookmark> {
+class BookmarkMapper(
+    val quotationRepository: QuotationRepository,
+    val quotationMapper: QuotationMapper,
+) : Mapper<BookmarkEntity, Bookmark> {
     override fun toDomain(entity: BookmarkEntity): Bookmark {
         return Bookmark(
             id = entity.id,
             name = entity.name,
             userId = entity.userId,
-            quotationIds = entity.quotationIds,
+            quotations = quotationMapper.toDomains(quotationRepository.findAllById(entity.quotationIds)),
             visibility = entity.visibility,
             icon = entity.icon,
             createdTime = entity.createdTime,
@@ -23,7 +27,7 @@ class BookmarkMapper : Mapper<BookmarkEntity, Bookmark> {
             id = domain.id,
             name = domain.name,
             userId = domain.userId,
-            quotationIds = domain.quotationIds,
+            quotationIds = domain.quotations.map { it.id },
             visibility = domain.visibility,
             icon = domain.icon,
             createdTime = domain.createdTime,
--- src/main/kotlin/wisoft/io/quotation/application/port/out/quotation/GetQuotationListPort.kt
+++ src/main/kotlin/wisoft/io/quotation/application/port/out/quotation/GetQuotationListPort.kt
@@ -4,9 +4,12 @@ import wisoft.io.quotation.adaptor.out.persistence.entity.view.QuotationRankView
 import wisoft.io.quotation.application.port.`in`.quotation.GetQuotationListUseCase
 import wisoft.io.quotation.application.port.`in`.quotation.GetQuotationRankUseCase
 import wisoft.io.quotation.domain.Quotation
+import java.util.*
 
 interface GetQuotationListPort {
     fun getQuotationList(request: GetQuotationListUseCase.GetQuotationListRequest): List<Quotation>
+
+    fun getQuotationListByIds(ids: List<UUID>): List<Quotation>
 
     fun getQuotationLank(request: GetQuotationRankUseCase.GetQuotationRankRequest): List<QuotationRankView>
 }
