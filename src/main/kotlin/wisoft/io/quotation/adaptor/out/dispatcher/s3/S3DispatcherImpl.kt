57cac8ae39af6505d483bbe982a144e8acb9d06f
Chanyeol Park <1qkrcksduf1@naver.com>
2024-09-04 19:05:42 +0900

    fix: bucket

--- src/main/kotlin/wisoft/io/quotation/adaptor/out/dispatcher/s3/S3DispatcherImpl.kt
+++ src/main/kotlin/wisoft/io/quotation/adaptor/out/dispatcher/s3/S3DispatcherImpl.kt
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.model.AmazonS3Exception
 import com.amazonaws.services.s3.model.ObjectMetadata
 import mu.KotlinLogging
 import org.apache.commons.codec.binary.Base64
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import wisoft.io.quotation.exception.error.ProfileMetadataValidationException
 import wisoft.io.quotation.exception.error.S3ObjectException
@@ -18,11 +19,14 @@ import javax.imageio.ImageIO
 class S3DispatcherImpl(
     private val amazonS3Client: AmazonS3,
 ) : S3Dispatcher {
+    @Value("\${cloud.aws.s3.bucket}")
+    val bucket: String = ""
     val logger = KotlinLogging.logger {}
     val bucketName: String = readYmlFile().cloud.aws.s3.bucket
 
     override fun createProfileImage(base64Image: String): String {
         return runCatching {
+            logger.info { "buck: $bucket" }
             logger.info { "bucketName: $bucketName" }
             val imageBytes = Base64.decodeBase64(base64Image)
 
@@ -41,7 +45,7 @@ class S3DispatcherImpl(
             metadata.contentLength = imageBytes.size.toLong()
             val imageId = UUID.randomUUID().toString()
             val inputStream = ByteArrayInputStream(imageBytes)
-            amazonS3Client.putObject(bucketName, imageId, inputStream, metadata)
+            amazonS3Client.putObject(bucket, imageId, inputStream, metadata)
             imageId
         }.onFailure {
             logger.error { "uploadS3Image fail" }
