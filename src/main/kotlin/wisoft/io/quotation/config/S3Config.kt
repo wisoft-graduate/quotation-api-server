4c014a5fd7f74133989c1e53f4d17ac507e3c3ef
Kukjun Lee <63409722+kukjun@users.noreply.github.com>
2024-09-04 20:50:49 +0900

    fix: update mapping application.yaml

--- src/main/kotlin/wisoft/io/quotation/config/S3Config.kt
+++ src/main/kotlin/wisoft/io/quotation/config/S3Config.kt
@@ -4,16 +4,25 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import wisoft.io.quotation.util.JWTUtil.readYmlFile
 
 @Configuration
 class S3Config(
-    private val accessKey: String = readYmlFile().cloud.aws.credentials.accessKey,
-    private val secretKey: String = readYmlFile().cloud.aws.credentials.secretKey,
-    private val region: String = readYmlFile().cloud.aws.region.static,
+//    private val accessKey: String = readYmlFile().cloud.aws.credentials.accessKey,
+//    private val secretKey: String = readYmlFile().cloud.aws.credentials.secretKey,
+//    private val region: String = readYmlFile().cloud.aws.region.static,
 ) {
+    @Value("\${cloud.aws.credentials.accessKey}")
+    lateinit var accessKey: String
+
+    @Value("\${cloud.aws.credentials.secretKey}")
+    lateinit var secretKey: String
+
+    @Value("\${cloud.aws.region.static}")
+    lateinit var region: String
+
     @Bean
     fun amazonS3Client(): AmazonS3 {
         return AmazonS3ClientBuilder.standard()
